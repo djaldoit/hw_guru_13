e9e7adc219983be0bf160b0a4822324e83e9bafa
Dzhalal <djaldoit@gmai.com>
2024-05-03 21:25:04 +0300

    homework3

--- test_browser.py
+++ test_browser.py
@@ -7,7 +7,7 @@ def test_google_search():
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_google_filed_search():
+def test_google_search_fail():
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.no.text('asdfasdfasdf'))
+    browser.element('[name="q"]').should(be.blank).type('439p8th0394guijnerpwoighnjsr;edofjhkbn').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
